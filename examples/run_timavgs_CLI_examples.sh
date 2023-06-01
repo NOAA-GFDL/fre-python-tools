9f2477dd0edffbc170eaf36b2955e99f8f751181
Ian Laflotte <ilaflott@gmail.com>
2023-06-01 11:35:43 -0400

    CLI example scripts shuffled around to base-level examples/ dir.
    some clean up of example scripts- clearer organization
    
    put back in fre-nctools example

---
+++ examples/run_timavgs_CLI_examples.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# quick shell script that generates time-averaged output of a test file
+# for various tests against the generate_time_averages python module
+# to use this script, cd to the fre-python-tools dir and do
+#
+#     source examples/run_timavgs_CLI_examples.sh
+#
+# the script should produce three new netcdf files containing the averages
+source $PWD/examples/timavgs_CLI_example_funcs.sh
+
+PP_COMP=atmos
+VAR=LWP
+TESTFILE_DIR=$PWD/tests/time_avg_test_files
+TESTFILE=$TESTFILE_DIR/${PP_COMP}.197901-198312.${VAR}.nc
+
+if [ -f $TESFILE ] ; then
+	
+	echo ""
+	echo "--------------------------------------------------"	
+	echo "target input file="
+	echo "$TESTFILE"
+	echo ""
+	echo "--------------------------------------------------"	
+	echo "running cdo timavg examples"	
+	cdo_timmean_ex
+	echo ""
+	echo "--------------------------------------------------"	
+	echo "running fre-nctools timavg examples"	
+	fre_nctools_timavg_ex
+	return 0
+else
+	echo "failed to run CLI examples"
+fi
+
+return 1
--- examples/timavgs_CLI_example_funcs.sh
+++ examples/timavgs_CLI_example_funcs.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-PP_COMP=atmos
-VAR=LWP
-TESTFILE_DIR=./tests/time_avg_test_files
-TESTFILE=$TESTFILE_DIR/${PP_COMP}.197901-198312.${VAR}.nc
-
-#cdo timavg
 function cdo_timmean_ex(){
 
 	#avg the variable at each lat/lon point over each month --> 12 #'s per lat/lon point (one per month)
@@ -23,7 +17,20 @@ function cdo_timmean_ex(){
 	## avg the variable at each lat/lon point over all points in time --> one # per lat/lon point 
 	outfile=$TESTFILE_DIR/cdo_timmean_CLI_test_${PP_COMP}_${VAR}_1979_5y.nc
 	if [ -f $outfile ] ; then rm -f $outfile ; fi
-	cdo timmean $TESTFILE $outfile	
+	cdo timmean $TESTFILE $outfile
+	unset outfile
 
+	return 0
+}
+
+function fre_nctools_timavg_ex(){
+	#if you already have fre-nctools, this step is unnecessary
+	module load fre-nctools
+
+	outfile=$TESTFILE_DIR/fre_nctools_timavg_CLI_test_${PP_COMP}_${VAR}_1979_5y.nc
+	if [ -f $outfile ] ; then rm -f $outfile ; fi
+	timavg.csh -r8 -mb -o $outfile $TESTFILE
+	unset outfile
+	
 	return 0
 }
